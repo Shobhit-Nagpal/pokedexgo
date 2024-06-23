7806c8f0d6489bc3b1530d20483b2f8d87698736
Shobhit-Nagpal <shobhitsnagpal@gmail.com>
2024-06-23 12:54:15 +0530

    change: bash script

--- main.sh
+++ main.sh
@@ -1,1 +1,19 @@
-go build && ./pokedexgo
+#!/bin/bash
+
+# Build the Go project
+go build
+
+# Get the name of the binary (assuming it's the same as the directory name)
+binary_name="pokedexgo"
+
+# Check if 'bin' directory exists, create it if it doesn't
+if [ ! -d "bin" ]; then
+    mkdir bin
+fi
+
+# Move the binary to the 'bin' directory
+mv "pokedexgo" bin/
+
+echo "Build complete. Binary moved to bin/pokedexgo"
+
+./bin/pokedexgo
